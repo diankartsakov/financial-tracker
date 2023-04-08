9952c30aa81be6af781e0d374c3d1dd3708e5e85
Alexander Ivanov <alexanderivanov2277@gmail.com>
2023-04-08 16:14:49 +0300

    create register form

--- src/components/RegisterForm.js
+++ src/components/RegisterForm.js
@@ -1,16 +1,87 @@
-
-import { Button, Checkbox, Form, Input } from 'antd';
-import './RegisterForm.css';
+import { Button, Checkbox, Form, Input } from "antd";
+import "./RegisterForm.css";
 
 export default function RegisterForm() {
+  const onFinish = (values) => {
+    console.log("Success:", values);
+  };
+  const onFinishFailed = (errorInfo) => {
+    console.log("Failed:", errorInfo);
+  };
 
+  return (
+    <div className="registerContainer">
+      <div className="registerFormDiv">
+        <h3>Register</h3>
+        <Form
+          name="basic"
+          labelCol={{
+            span: 8,
+          }}
+          wrapperCol={{
+            span: 16,
+          }}
+          style={{
+            maxWidth: 700,
+          }}
+          initialValues={{
+            remember: true,
+          }}
+          onFinish={onFinish}
+          onFinishFailed={onFinishFailed}
+          autoComplete="off"
+        >
+          <Form.Item
+            label="Username"
+            name="username"
+            rules={[
+              {
+                required: true,
+                message: "Please input your username!",
+              },
+            ]}
+          >
+            <Input />
+          </Form.Item>
 
-    return (<div className='registerContainer'>
-        <div className='registerFormDiv'>
-            <h3>Register</h3>
-            
-        </div>
+          <Form.Item
+            label="Password"
+            name="password"
+            rules={[
+              {
+                required: true,
+                message: "Please input your password!",
+              },
+            ]}
+          >
+            <Input.Password />
+          </Form.Item>
+
+          <Form.Item
+            label="Confirm Password"
+            name="rePassword"
+            rules={[
+              {
+                required: true,
+                message: "Please confirm your password!",
+              },
+            ]}
+          >
+            <Input.Password />
+          </Form.Item>
+
+          <Form.Item
+            wrapperCol={{
+              offset: 8,
+              span: 16,
+            }}
+          >
+            <Button type="primary" htmlType="submit">
+              Register
+            </Button>
+          </Form.Item>
+        </Form>
+      </div>
     </div>
-
-    );
-};
+  );
+}
