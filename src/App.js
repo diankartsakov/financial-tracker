b979f24b793a8c70055bb07dc3534c0bac32017d
Alexander Ivanov <alexanderivanov2277@gmail.com>
2023-04-09 00:31:47 +0300

    wrap app with auth user provider(context) for entire app

--- src/App.js
+++ src/App.js
@@ -3,20 +3,23 @@ import { BrowserRouter, Routes, Route, Link, Navigate } from 'react-router-dom';
 import HomePage from './pages/HomePage';
 import Login from './pages/LoginPage';
 import Register from './pages/RegisterPage';
-
+import HomeNavigation from './components/HomeNavigation';
+import { AuthUserProvied } from './firebase/auth';
 
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route index element={<Navigate to={'/home'} />}></Route>
-        <Route path={'/home'} element={<HomePage />}></Route>
-        <Route path={'/login'} element={<Login />}></Route>
-        <Route path={'/register'} element={<Register />}></Route>
-        <Route path={'*'} element={<div>PAGE NOT FOUND !</div>}></Route>
-      </Routes>
-    </BrowserRouter>
-
+    <AuthUserProvied>
+          <BrowserRouter>
+            <HomeNavigation/>
+            <Routes>
+              <Route index element={<Navigate to={'/home'} />}></Route>
+              <Route path={'/home'} element={<HomePage />}></Route>
+              <Route path={'/login'} element={<Login/>}></Route>
+              <Route path={'/register'} element={<Register />}></Route>
+              <Route path={'*'} element={<div>PAGE NOT FOUND !</div>}></Route>
+            </Routes>
+          </BrowserRouter>
+    </AuthUserProvied>
   );
 }
 
