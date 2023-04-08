a817b17451c4f8f73e28ab680f454b16faf15d90
Alexander Ivanov <alexanderivanov2277@gmail.com>
2023-04-08 13:00:06 +0300

    configurate sign-in method for authentication and import it to firabase.js and export auth for use in app

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -1,9 +1,6 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import { getAuth } from "firebase/auth";
 
-// Your web app's Firebase configuration
 const firebaseConfig = {
   apiKey: "AIzaSyAXhJHIgKOiv-yG7nhkjZHR7TjaTfna1fg",
   authDomain: "firabase-demo-99588.firebaseapp.com",
@@ -15,3 +12,8 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+
+export {
+    auth,
+}
