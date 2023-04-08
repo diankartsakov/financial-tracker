70eabfa57729d828bcdc8dec3dd3575957702630
Alexander Ivanov <alexanderivanov2277@gmail.com>
2023-04-08 12:22:35 +0300

    create config folder for firebase configuration. add firebase.js with configuration for firebase.

---
+++ src/config/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAXhJHIgKOiv-yG7nhkjZHR7TjaTfna1fg",
+  authDomain: "firabase-demo-99588.firebaseapp.com",
+  projectId: "firabase-demo-99588",
+  storageBucket: "firabase-demo-99588.appspot.com",
+  messagingSenderId: "932966757594",
+  appId: "1:932966757594:web:1a0a5eedb58e466cd161e3"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
