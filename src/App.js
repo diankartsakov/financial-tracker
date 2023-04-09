7fd35824d50ebec3f15a5c011edffd98e6b859bb
Alexander Ivanov <alexanderivanov2277@gmail.com>
2023-04-09 19:33:42 +0300

    implement AuthUserProvider from useContext for firebase authentication wrap App component and add loading while firebase load

--- src/App.js
+++ src/App.js
@@ -1,26 +1,35 @@
 import './App.css';
-import { BrowserRouter, Routes, Route, Link, Navigate } from 'react-router-dom';
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 import HomePage from './pages/HomePage';
 import Login from './pages/LoginPage';
 import Register from './pages/RegisterPage';
 import HomeNavigation from './components/HomeNavigation';
-import { AuthUserProvied } from './firebase/auth';
+import { useAuth } from './firebase/auth';
+
 
 function App() {
-  return (
-    <AuthUserProvied>
-          <BrowserRouter>
-            <HomeNavigation/>
-            <Routes>
-              <Route index element={<Navigate to={'/home'} />}></Route>
-              <Route path={'/home'} element={<HomePage />}></Route>
-              <Route path={'/login'} element={<Login/>}></Route>
-              <Route path={'/register'} element={<Register />}></Route>
-              <Route path={'*'} element={<div>PAGE NOT FOUND !</div>}></Route>
-            </Routes>
-          </BrowserRouter>
-    </AuthUserProvied>
-  );
+    const { isLoading } = useAuth();
+    return (
+        <>
+            {   
+                isLoading 
+                ? 
+                    <div>Loading... </div>
+                :       
+                    <BrowserRouter>
+                        <HomeNavigation/>
+                        <Routes>
+                            <Route index element={<Navigate to={'/home'} />}></Route>
+                            <Route path={'/home'} element={<HomePage />}></Route>
+                            <Route path={'/login'} element={<Login/>}></Route>
+                            <Route path={'/register'} element={<Register />}></Route>
+                            <Route path={'*'} element={<div>PAGE NOT FOUND !</div>}></Route>
+                        </Routes>
+                    </BrowserRouter>
+            }
+        </>
+        
+    );
 }
 
 export default App;
