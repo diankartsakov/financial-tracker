d718c2e070725b475afb9cda9d3acc9f72b6186d
diankartsakov <dian.kartsakov@gmail.com>
2023-04-10 23:55:06 +0300

    Adding new doc to FireStore collection

--- src/services/UserManager.js
+++ src/services/UserManager.js
@@ -1,80 +1,100 @@
 // import axios from "axios";
 
-import { db } from '../firebase/firebase.js';
+import { auth, db } from '../firebase/firebase.js';
+import { addDoc, collection } from "firebase/firestore"; 
 
 class UserManager {
 
     // Add a new transaction to the "transactions" collection
 
-    addTransaction = (accountId, amount, category, type, fromAccountId) => {
+     addTransaction = async (accountId, amount, category, type, fromAccountId) => {
         console.log(db);
+        console.log(auth);
 
-        if (type === 'Expense') {
+        try {
 
-            db.collection('transactions').add({
-                account_id: accountId,
-                amount: amount,
-                category: category,
-                type: type,
-                date: new Date(),
-            })
-                .then((docRef) => {
-                    console.log('Document written with ID: ', docRef.id);
-                })
-                .catch((error) => {
-                    console.error('Error adding document: ', error);
-                });
 
-        }
-        else if (type === 'Deposit') {
-
-            db.collection('transactions').add({
+            const docRef = await addDoc(collection(db, "transactions"), {
                 account_id: accountId,
                 amount: amount,
                 category: 'Card Deposit',
                 type: type,
-                date: new Date(),
-            })
-                .then((docRef) => {
-                    console.log('Document written with ID: ', docRef.id);
-                })
-                .catch((error) => {
-                    console.error('Error adding document: ', error);
-                });
+                date: new Date()
+            });
+          
+            console.log("Document written with ID: ", docRef.id);
+          } catch (e) {
+            console.error("Error adding document: ", e);
+          }
 
-        }
-        else {
 
-            db.collection('transactions').add({
-                account_id: accountId,
-                from_account_id: fromAccountId,
-                amount: amount,
-                category: 'Internal Transfer',
-                type: type,
-                date: new Date(),
-            })
-                .then((docRef) => {
-                    console.log('Document written with ID: ', docRef.id);
-                })
-                .catch((error) => {
-                    console.error('Error adding document: ', error);
-                });
 
-            db.collection('transactions').add({
-                account_id: fromAccountId,
-                to_account_id: accountId,
-                amount: amount,
-                category: 'Internal Transfer',
-                type: type,
-                date: new Date(),
-            })
-                .then((docRef) => {
-                    console.log('Document written with ID: ', docRef.id);
-                })
-                .catch((error) => {
-                    console.error('Error adding document: ', error);
-                });
-        }
+        // if (type === 'Expense') {
+
+        //     db.collection('transactions').add({
+        //         account_id: accountId,
+        //         amount: amount,
+        //         category: category,
+        //         type: type,
+        //         date: new Date(),
+        //     })
+        //         .then((docRef) => {
+        //             console.log('Document written with ID: ', docRef.id);
+        //         })
+        //         .catch((error) => {
+        //             console.error('Error adding document: ', error);
+        //         });
+
+        // }
+        // else if (type === 'Deposit') {
+
+        //     db.collection('transactions').add({
+        //         account_id: accountId,
+        //         amount: amount,
+        //         category: 'Card Deposit',
+        //         type: type,
+        //         date: new Date(),
+        //     })
+        //         .then((docRef) => {
+        //             console.log('Document written with ID: ', docRef.id);
+        //         })
+        //         .catch((error) => {
+        //             console.error('Error adding document: ', error);
+        //         });
+
+        // }
+        // else {
+
+        //     db.collection('transactions').add({
+        //         account_id: accountId,
+        //         from_account_id: fromAccountId,
+        //         amount: amount,
+        //         category: 'Internal Transfer',
+        //         type: type,
+        //         date: new Date(),
+        //     })
+        //         .then((docRef) => {
+        //             console.log('Document written with ID: ', docRef.id);
+        //         })
+        //         .catch((error) => {
+        //             console.error('Error adding document: ', error);
+        //         });
+
+        //     db.collection('transactions').add({
+        //         account_id: fromAccountId,
+        //         to_account_id: accountId,
+        //         amount: amount,
+        //         category: 'Internal Transfer',
+        //         type: type,
+        //         date: new Date(),
+        //     })
+        //         .then((docRef) => {
+        //             console.log('Document written with ID: ', docRef.id);
+        //         })
+        //         .catch((error) => {
+        //             console.error('Error adding document: ', error);
+        //         });
+        // }
 
     };
 }
