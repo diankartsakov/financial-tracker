2946a1f29e6e5cb0b01d5862ba51c5db5520671d
Alexander Ivanov <alexanderivanov2277@gmail.com>
2023-04-12 02:39:52 +0300

    create firestore accounts file for functions about manage data from firestore accounts and implement function for get all accounts

---
+++ src/services/firebaseFirestoreAccounts.js
@@ -0,0 +1,22 @@
+import {db} from "../firebase/firebase";
+import { collection, query, where, getDocs } from "firebase/firestore";
+
+async function getUserAccounts(uid) {
+    const accounts = [];
+
+    const q = query(collection(db, "accounts"), where("uid", "==", uid));
+
+    const querySnapshot = await getDocs(q);
+
+    querySnapshot.forEach((doc) => {
+    // doc.data() is never undefined for query doc snapshots
+        // console.log(doc.id, " => ", doc.data());
+        accounts.push(doc.id);
+    });
+    
+    return accounts;
+}
+
+export {
+    getUserAccounts,
+}
