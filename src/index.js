47b1a4196608903e48a09d9dc546a6f96fe73827
Alexander Ivanov <alexanderivanov2277@gmail.com>
2023-04-18 00:28:01 +0300

    clean unused files

--- src/index.js
+++ src/index.js
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import { AuthUserProvied } from './firebase/auth';
-import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -13,4 +12,3 @@ root.render(
 
 );
 
-reportWebVitals();
