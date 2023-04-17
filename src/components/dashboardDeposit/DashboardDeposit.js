ba56b1d0008bc5dad3dcd7ad80285f0d5ee42838
diankartsakov <dian.kartsakov@gmail.com>
2023-04-17 16:12:21 +0300

    Steps timeline added along with additional deposit steps

--- src/components/dashboardDeposit/DashboardDeposit.js
+++ src/components/dashboardDeposit/DashboardDeposit.js
@@ -1,11 +1,12 @@
 import React, { useState } from 'react';
-import { Form, Input, Radio, Button, Modal } from 'antd';
-import { Row, Col } from 'react-bootstrap';
+import { Form, Input, Radio, Button, Modal, Steps } from 'antd';
 import TransferDropdown from '../transferDropdown/TransferDropdown';
 import { useDash } from "../../pages/dashboardPage/DashboardProvider";
 import accountManager from '../../services/AccountManager';
 import { getUserAccountsFullInfo } from '../../services/firebaseFirestoreAccounts';
 import { useAuth } from '../../firebase/auth';
+import './DashboardDeposit.scss';
+
 
 export default function DashboardDeposit() {
   const [depositType, setDepositType] = useState('card');
@@ -13,10 +14,12 @@ export default function DashboardDeposit() {
   const [fromAccount, setFromAccount] = useState('');
   const [modalVisible, setModalVisible] = useState(false);
   const [modalMessage, setmodalMessage] = useState([]);
+  const [confirmationData, setConfirmationData] = useState({});
+  const [currentStep, setCurrentStep] = useState(0);
   const [form] = Form.useForm();
 
-  const { accountId, currentAccountName, updateAccountsArr } = useDash();
-  const {authUser: {uid}} = useAuth();
+  const { accountId, currentAccountName, accountsArr, updateAccountsArr } = useDash();
+  const { authUser: { uid } } = useAuth();
 
   const handleDepositTypeChange = e => {
     setDepositType(e.target.value);
@@ -25,20 +28,20 @@ export default function DashboardDeposit() {
   const handleAmountChange = e => {
     const newAmount = e.target.value;
     if (newAmount < 0) {
-      setmodalMessage(['Invalid Amount','Please enter a valid amount.']);
+      setmodalMessage(['Invalid Amount', 'Please enter a valid amount.']);
       setModalVisible(true);
     } else {
       setAmount(newAmount);
     }
   };
 
-  const handleFromAccountSelect = (key) => {
-    setFromAccount(key);
+  const handleFromAccountSelect = (acc) => {
+    setFromAccount(acc);
   };
 
   const handleOk = () => {
     setModalVisible(false);
-    if(modalMessage[1] === 'Please enter a valid amount.'){
+    if (modalMessage[1] === 'Please enter a valid amount.') {
       form.resetFields(['amount']);
     }
 
@@ -46,71 +49,75 @@ export default function DashboardDeposit() {
 
   const handleCancel = () => {
     setModalVisible(false);
-    if(modalMessage[1] === 'Please enter a valid amount.'){
+    if (modalMessage[1] === 'Please enter a valid amount.') {
       form.resetFields(['amount']);
     }
   };
 
-  const handlePayButtonClick = async() => {
+  const handleContinueToCheckoutClick = async () => {
 
     const values = await form.validateFields();
 
     if (!values.amount) {
-        setmodalMessage(['Invalid Amount','Please enter a valid amount.']);
-        setModalVisible(true);
-        return;
+      setmodalMessage(['Invalid Amount', 'Please enter a valid amount.']);
+      setModalVisible(true);
+      return;
     }
 
+    if (depositType !== 'card' && !fromAccount) {
+      setmodalMessage(['Missing Information', 'Please select From Account.']);
+      setModalVisible(true);
+      return;
+
+    }
+    console.log(values);
+
+    if (values) {
+      setCurrentStep(1);
+      setConfirmationData(values);
+      
+    }
+
+
+  }; 
+
+  const handlePayButtonClick = async () => {
+
     if (depositType === 'card') {
-        await accountManager.initiateTransaction(currentAccountName, accountId, amount, 'Deposit', 'Card Deposit');
+      await accountManager.initiateTransaction(currentAccountName, accountId, amount, 'Deposit', 'Card Deposit');
     } else {
-        if (!fromAccount) {
-        setmodalMessage(['Missing Information','Please select From Account.']);
+      if (!fromAccount) {
+        setmodalMessage(['Missing Information', 'Please select From Account.']);
         setModalVisible(true);
         return;
-        }
-        await accountManager.initiateTransaction(currentAccountName, accountId, amount, 'Transfer', 'Internal Transfer', fromAccount);
+      }
+      await accountManager.initiateTransaction(currentAccountName, accountId, amount, 'Transfer', 'Internal Transfer', fromAccount.key);
     }
+
 
 
     const accountsFullInfo = await getUserAccountsFullInfo(uid);
     updateAccountsArr(accountsFullInfo);
-    //OLD
-    // form.validateFields().then(values => {
-    //     if (!values.amount) {
-    //         setmodalMessage(['Invalid Amount','Please enter a valid amount.']);
-    //         setModalVisible(true);
-    //         return;
-    //     }
 
-    //     if (depositType === 'card') {
-    //         accountManager.initiateTransaction(currentAccountName, accountId, amount, 'Deposit', 'Card Deposit');
-    //         console.log("hi deposit");
-    //         return true;
-    //     } else {
-    //         if (!fromAccount) {
-    //         setmodalMessage(['Missing Information','Please select From Account.']);
-    //         setModalVisible(true);
-    //         return;
-    //         }
-    //         accountManager.initiateTransaction(currentAccountName, accountId, amount, 'Transfer', 'Internal Transfer', fromAccount);
-    //         // return true;
-    //     }
-
-    //     console.log("transaction end");
-    // })
-    // .then(res => {
-    //     console.log(res);
-    //     if (res) {
-    //         getUserAccountsFullInfo(uid).then(res => {
-    //             console.log(res);
-    //         })
-    //     }
-    // });
   };
 
   return (
     <>
+      <Steps
+        size="small"
+        current={currentStep}
+        items={[
+          {
+            title: 'Select Deposit Option',
+          },
+          {
+            title: 'Confirm Transaction',
+          },
+          {
+            title: 'Successful Transaction',
+          },
+        ]}
+      />
       <Modal
         open={modalVisible}
         onOk={handleOk}
@@ -128,62 +135,108 @@ export default function DashboardDeposit() {
         {modalMessage[1]}
       </Modal>
 
-      <Form form={form} onFinish={handlePayButtonClick}>
-        <Form.Item label="Amount" name="amount" rules={[{ required: true, message: 'Please enter a valid amount' }]}>
-          <Input type="number" name="amount" onChange={handleAmountChange} />
-        </Form.Item>
-
-        <Form.Item label="Deposit Type" name="depositType" initialValue="card">
-          <Radio.Group onChange={handleDepositTypeChange} value={depositType}>
-            <Radio value="card">Card Deposit</Radio>
-            <Radio value="account">Transfer from Account</Radio>
-          </Radio.Group>
-        </Form.Item>
-
-        {depositType === 'card' && (
-          <>
-            <Row>
-              <Col xs={12} sm={6}>
-                <Form.Item label="Card Number" name="cardNumber" rules={[{ required: true, message: 'Please enter a valid card number' }]}>
-                  <Input type="text" maxLength={16} />
-                </Form.Item>
-              </Col>
-
-              <Col xs={12} sm={6}>
-                <Form.Item label="Expiration Date" name="expirationDate" rules={[{ required: true, message: 'Please enter a valid expiration date' }]}>
-                  <Input type="month" name="expirationDate" placeholder="MM/YY" />
-                </Form.Item>
-              </Col>
-            </Row>
-
-            <Row>
-              <Col xs={12} sm={6}>
-                <Form.Item label="CVV" name="cvv" rules={[{ required: true, message: 'Please enter a valid CVV' }]}>
-                  <Input type="number" name="cvv" />
-                </Form.Item>
-              </Col>
-
-              <Col xs={12} sm={6}>
-                <Form.Item label="Card Holder Name" name="cardHolderName" rules={[{ required: true, message: 'Please enter your full names.' }]}>
-                  <Input type="text" name="cardHolderName" />
-                </Form.Item>
-              </Col>
-            </Row>
-          </>
-        )}
-
-        {depositType === 'account' && (
-          <Form.Item label="From Account">
-            <TransferDropdown onSelect={handleFromAccountSelect}></TransferDropdown>
+      {currentStep === 0 && (<div className='deposit-form-details'>
+        <Form form={form} onFinish={handleContinueToCheckoutClick}>
+          <h3>Deposit Form</h3>
+          <Form.Item label="Amount" name="amount" rules={[{ required: true, message: 'Please enter a valid amount' }]}>
+            <Input type="number" name="amount" onChange={handleAmountChange} />
           </Form.Item>
-        )}
 
-        <Form.Item>
-          <Button type="primary" htmlType="submit">
-            Pay
-          </Button>
-        </Form.Item>
-      </Form>
+          <Form.Item label="Deposit Type" name="depositType" initialValue="card">
+            <Radio.Group onChange={handleDepositTypeChange} value={depositType}>
+              <Radio value="card">Card Deposit</Radio>
+              <Radio value="account">Transfer from Account</Radio>
+            </Radio.Group>
+          </Form.Item>
+
+          {depositType === 'card' && (
+            <>
+
+              <Form.Item label="Card Number" name="cardNumber" rules={[{ required: true, message: 'Please enter a valid card number' }]}>
+                <Input type="text" maxLength={16} />
+              </Form.Item>
+
+              <Form.Item label="Expiration Date" name="expirationDate" rules={[{ required: true, message: 'Please enter a valid expiration date' }]}>
+                <Input type="month" name="expirationDate" placeholder="MM/YY" />
+              </Form.Item>
+
+
+
+              <Form.Item label="CVV" name="cvv" rules={[{ required: true, message: 'Please enter a valid CVV' }]}>
+                <Input type="number" name="cvv" />
+              </Form.Item>
+
+
+
+              <Form.Item label="Card Holder Name" name="cardHolderName" rules={[{ required: true, message: 'Please enter your full names.' }]}>
+                <Input type="text" name="cardHolderName" />
+              </Form.Item>
+            </>
+          )}
+
+          {depositType === 'account' && (
+            <Form.Item label="From Account">
+              <TransferDropdown onSelect={handleFromAccountSelect} currentAcc = {fromAccount}></TransferDropdown>
+            </Form.Item>
+          )}
+
+          <Form.Item>
+            <Button type="primary" htmlType="submit">
+              Continue to Checkout
+            </Button>
+          </Form.Item>
+        </Form>
+      </div>)}
+      {currentStep === 1 && (
+        <div className='deposit-form-details'>
+          <Form 
+          //onFinish={()=>{}}
+          >
+            <Form.Item><h3>Confirm Transaction Details</h3></Form.Item>
+            <Form.Item>Amount: {`${Number(confirmationData.amount).toFixed(2)} BGN`}</Form.Item>
+            <Form.Item><p>Deposit Type: {confirmationData.depositType}</p></Form.Item>
+
+            {confirmationData.depositType === 'card' && (
+              <>
+                <Form.Item>
+                  <p>Card Number: {confirmationData.cardNumber}</p>
+                </Form.Item>
+                <Form.Item>
+                  <p>Expiration Date: {confirmationData.expirationDate}</p>
+
+                </Form.Item>
+                <Form.Item>
+                  <p>CVV: {confirmationData.cvv}</p>
+
+                </Form.Item>
+                <Form.Item>
+                  <p>Card Holder Name: {confirmationData.cardHolderName}</p>
+                </Form.Item>
+
+              </>
+            )}
+            {confirmationData.depositType === 'account' && (
+
+              <Form.Item>
+                <p>From Account: {fromAccount.label}</p>
+              </Form.Item>
+
+            )}
+            <Form.Item>
+
+              <Button type="secondary" onClick={() => setCurrentStep(0)}>
+                Back
+              </Button>
+              <Button type="secondary" onClick={handlePayButtonClick}>
+                Confirm Payment
+              </Button>
+
+            </Form.Item>
+          </Form>
+        </div>
+
+      )}
+
     </>
   );
 };
