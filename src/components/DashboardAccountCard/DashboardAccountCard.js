13c4070f44d0bab95eab5a7793b1dd75a0384682
diankartsakov <dian.kartsakov@gmail.com>
2023-04-17 20:18:45 +0300

    adding classes for styling

--- src/components/DashboardAccountCard/DashboardAccountCard.js
+++ src/components/DashboardAccountCard/DashboardAccountCard.js
@@ -3,13 +3,13 @@ import { Card, Button } from 'antd';
 import { useDash } from '../../pages/dashboardPage/DashboardProvider';
 import { getAccount } from '../../services/firebaseFirestoreAccounts';
 import { Link } from 'react-router-dom';
+import './DashboardAccountCard.scss';
 
 export default function DashboardAccountCard({ currency = 'BGN',  }) {
     const { accountId, accountsArr, currentAccountName } = useDash(); 
     const [isLoading, setIsLoading] = useState(true);
     const [balance, setBalance] = useState(0);
     const [accountName, setAccountName] = useState('');
-    const balanceColor = balance >= 0 ? 'green' : 'red';
 
     useEffect(() => {
 
@@ -21,40 +21,29 @@ export default function DashboardAccountCard({ currency = 'BGN',  }) {
         }
     }, [accountId]);
 
-    const balanceStyle = {
-        fontSize: '32px',
-        fontWeight: 'bold',
-        color: balanceColor,
-        textShadow: `0 0 10px ${balanceColor}`,
-        textAlign: 'center',
-    };
-    const accountNameStyle = {
-        fontSize: '24px',
-        fontWeight: 'bold',
-        textTransform: 'uppercase',
-        textAlign: 'center',
-    };
 
     return (
         <>
         {
             accountsArr?.length && accountId
             ?
-            <Card>
-            <div style={balanceStyle}>{balance.toFixed(2) + " " + currency}</div>
-            <div style={accountNameStyle}>{accountName}</div>
-            <div style={{ display: 'flex', justifyContent: 'center', gap: '10px' }}>
+            <div className='da-account-form-details'>
+            <Card className='da-account-form-card'>
+            <div className='da-account-form-balance'>{balance.toFixed(2) + " " + currency}</div>
+            <div className='da-account-form-name'>{accountName}</div>
+            <div className='da-account-form-buttons'>
             <Link to="/dashboard/deposit">
-            <Button type="primary" >Deposit</Button>
+            <Button className='da-btn' type="primary" >Deposit</Button>
             </Link>
 
             <Link to="/dashboard/expense">
-                <Button type="primary" >Expense</Button>
+                <Button className='da-btn' type="primary" >Expense</Button>
             </Link>
             </div>
             </Card>
+            </div>
             : 
-            <div>NO ACCOUNTS. YOU NEEED TO CREATE ONE</div>
+            <div className='da-account-form-no-account'>NO ACCOUNTS. YOU NEEED TO CREATE ONE</div>
         }
 
       </>
