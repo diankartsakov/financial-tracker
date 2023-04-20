25e11accb39384d12821cfd2f03186b33b663e90
Alexander Ivanov <alexanderivanov2277@gmail.com>
2023-04-20 15:56:47 +0300

    add generator for deposits

--- src/assests/utils/dataGenerator.js
+++ src/assests/utils/dataGenerator.js
@@ -2,7 +2,7 @@ function getExpenses(days=30) {
     const categories = ['Shops', 'Food', 'Hobbies', 'Credit', 'Taxes', 'Car'];
     const accounts = [
       { reportAccountId: 'A2ejBSOPsVU3hxgy6ql9', reportAccountName: 'TEST NEW ACCOUNT' },
-      // { reportAccountId: 'A2ejBSOPsVU3hxgy6ql9', reportAccountName: 'TEST NEW ACCOUNT' },
+
       { reportAccountId: 'JA72tSY61CTQXt9oBMNN', reportAccountName: 'acc' },
     ];
     const expenses = [];
@@ -41,6 +41,47 @@ function getExpenses(days=30) {
 }
 
 
+function getCardDeposits(days=30) {
+  const categories = ['Card Deposit'];
+  const accounts = [
+    { reportAccountId: 'A2ejBSOPsVU3hxgy6ql9', reportAccountName: 'TEST NEW ACCOUNT' },
+    { reportAccountId: 'JA72tSY61CTQXt9oBMNN', reportAccountName: 'acc' },
+  ];
+  const cardDeposits = [];
+
+  for (let i = 0; i < days; i++) {
+    const categoryIndex = Math.floor(Math.random() * categories.length);
+    const category = categories[categoryIndex];
+      
+    const accountIndex = Math.floor(Math.random() * accounts.length);
+    const account = accounts[accountIndex];
+
+    const day = Math.floor(Math.random() * 30) + 1; // Random day between 1-30
+    const month = Math.floor(Math.random() * 2) + 2;
+    const date = new Date(2023, month, day, 12, 0, 0); // April 2023 at 12pm
+
+    const amount = Math.floor(Math.random() * 1000) / 100; // Random amount between 0.00-9.99
+    const amountString = amount.toFixed(2) + ' BGN';
+
+    const cardDeposit = {
+      accountId: account.reportAccountId,
+      accountName: account.reportAccountName,
+      amount,
+      amountString,
+      category,
+      date,
+      id: `cardDeposit${i}`,
+      type: 'Deposit',
+    };
+
+    cardDeposits.push(cardDeposit);
+  }
+
+  return cardDeposits;
+}
+
+
 export {
     getExpenses,
+    getCardDeposits,
 }
