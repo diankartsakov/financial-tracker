e336680092e9cc8dd31dcc5617ed378593cc6352
Alexander Ivanov <alexanderivanov2277@gmail.com>
2023-04-24 00:46:01 +0300

    refactor header add expense dropdown and amount status

--- src/components/dashboardExpense/DashboardExpense.js
+++ src/components/dashboardExpense/DashboardExpense.js
@@ -4,11 +4,13 @@ import { useDash } from "../../pages/dashboardPage/DashboardProvider"
 import { addCategory } from "../../services/firebaseFirestoreCategories";
 import CategoryFormModal from "../categoryFormModal/CategoryFormModal";
 import ExpenseCategory from "../expenseCategory/ExpenseCategory";
+import moneyIMG from "../../assests/images/amount-logo.png"
 import "./dashboardExpense.scss";
-import { Link } from "react-router-dom";
+import ExpenseDropdown from "../expenseDropdown/ExpenseDropdown";
+
 
 export default function DashboardExpense() {
-    const {currentAccountName, categories, updateCategories} = useDash();
+    const {currentAccountName, accountId, categories, accountsArr, updateCategories} = useDash();
     const [isLoading, setIsLoading ] = useState(true);
 
     useEffect(() => {
@@ -16,19 +18,32 @@ export default function DashboardExpense() {
     }, []);
 
     return (
-        <>  <h1 className="expense-account-heading"> User expense Categories</h1>
-            <h1 className="expense-account-heading">Expense for: <Link to="/dashboard/accounts" className="expense-account-name">{currentAccountName}</Link></h1>
+        <>  
+            <header className="expense-header">
+                <h1 className="expense-account-heading"> User Expense Categories</h1>
+                <h2 className="expense-account-sub-heading">
+                    Expense current account:  
+                    <ExpenseDropdown/>
+                    <div className="account-amount-wrapper">
+                        <img src={moneyIMG} alt="money logo"/>
+                        <p>{accountsArr.find(acc => acc.accountId === accountId).amount} BGN</p>
+                    </div>
+                </h2>
+            </header>
+           
             <CategoryFormModal onSubmit={addCategory}/>
-            <div className="categoriesWrapper">{
-                isLoading ?
-                <div>Loading...</div>
-                :
-                categories.length ?
-                categories.map(category => <ExpenseCategory key={category.id} data={category} updateCategories={updateCategories}/> )
-                :
-                "NO CATEGORIES"
-                
-            }</div>
+            <div className="categoriesWrapper">
+                {
+                    isLoading ?
+                    <div>Loading...</div>
+                    :
+                    categories.length ?
+                    categories.map(category => <ExpenseCategory key={category.id} data={category} updateCategories={updateCategories}/> )
+                    :
+                    "NO CATEGORIES"
+                }
+            </div>
+            
         </>
     )
 }
