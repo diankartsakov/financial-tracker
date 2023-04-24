577ef12ebcbc707edd2fe29c5422b5b80555b5ef
Alexander Ivanov <alexanderivanov2277@gmail.com>
2023-04-24 23:58:51 +0300

    implement new sections to homepage

--- src/pages/homePage/HomePage.js
+++ src/pages/homePage/HomePage.js
@@ -1,6 +1,8 @@
 import "./homePage.scss";
 import startGif from "../../assests/images/imageedit_4_9427188444.gif";
-import underConstructionIMG from "../../assests/images/under-construction_geek_man.svg"
+import planner from "../../assests/images/planner.png"
+import whereMoneyGoIMG from "../../assests/images/where-money-go.png"
+import { Link } from "react-router-dom";
 
 export default function HomePage() {
     return (
@@ -21,10 +23,36 @@ export default function HomePage() {
                     </svg>
                 </div>    
             </div>    
-            <div className="homePage">
-                <h2>Home Page</h2>
-                <div className="underConstruction">
-                    <img src={underConstructionIMG} alt="" />
+            <div className="info-where">
+                <div className="info-where-wrapper">
+                    <div className="info-where-description">
+                        <h2 className="info-where-description-title">Stay on Top of Your Expenses</h2>
+                        <p className="info-where-description-text">Smart Finance makes it easy to create a budget and track your spending, so you can make smarter financial decisions.</p>
+                        <Link className="link-register-button" to="/register">Start Tracking Your Spending Now</Link>
+                    </div>
+                    <div className="info-where-img-wrapper">
+                        <img src={whereMoneyGoIMG} alt="logo where money go"/>
+                    </div>
+                </div>
+            </div>
+            <div className="slogan">
+                <div className="slogan-wrapper">
+                        <h2 className="slogan-title">Spend less time stressing, and more time living with our finance app</h2>
+                        <p className="slogan-text">Our finance app takes the hassle out of managing your money, so you can spend less time stressing about finances and more time enjoying life. With our easy-to-use interface and powerful features, you'll have everything you need to stay on top of your finances and achieve your goals.</p>
+                </div>
+            </div>
+
+            {/* Spend less time stressing, and more time living with our finance app. */}
+            <div className="info-where">
+                <div className="info-where-wrapper">
+                    <div className="info-where-description">
+                        <h2 className="info-where-description-title">Plan smarter, spend better</h2>
+                        <p className="info-where-description-text">Our finance app helps you plan smarter by creating a personalized budget and tracking your expenses. With these powerful tools, you can make informed financial decisions and spend better, ultimately achieving your financial goals and living the life you want.</p>
+                        <Link className="link-register-button" to="/register">Plan smarter, spend better and succeed Now</Link>
+                    </div>
+                    <div className="info-where-img-wrapper">
+                        <img src={planner} className="planner" alt="logo where money go"/>
+                    </div>
                 </div>
             </div>
         </>
