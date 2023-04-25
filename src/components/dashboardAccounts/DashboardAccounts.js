6f03235716a9dc86e4d032c1341d481f241c6f27
Alexander Ivanov <alexanderivanov2277@gmail.com>
2023-04-25 20:44:15 +0300

    implement modal info after successful account creation

--- src/components/dashboardAccounts/DashboardAccounts.js
+++ src/components/dashboardAccounts/DashboardAccounts.js
@@ -1,5 +1,6 @@
-import { useState } from 'react';
-import { Modal, Form, Input, Button } from 'antd';
+import { useEffect, useState } from 'react';
+import { Modal, Form, Input, Button, Result } from 'antd';
+import { SmileOutlined } from '@ant-design/icons';
 import { useAuth } from "../../firebase/auth";
 import { useDash } from "../../pages/dashboardPage/DashboardProvider";
 import accountManager from '../../services/AccountManager';
@@ -7,8 +8,9 @@ import AccountsDrowpdown from '../accountDropdown/AccountsDropdown';
 import DashboardAccountCard from '../DashboardAccountCard/DashboardAccountCard';
 import './DashboardAccounts.scss';
 
-function NewAccountModal({ onCreate }) {
+function NewAccountModal({ onCreate, isFirst=false, setIsNoAccounts }) {
   const [isModalOpen, setIsModalOpen] = useState(false);
+  const [isComplete, setIsComplete] = useState(false);
   const [form] = Form.useForm();
 
   const showModal = () => {
@@ -16,27 +18,41 @@ function NewAccountModal({ onCreate }) {
   };
 
   const hideModal = () => {
+    setIsNoAccounts(true)
     setIsModalOpen(false);
   };
 
   const handleCreate = () => {
     form.validateFields().then((values) => {
       onCreate(values);
-      hideModal();
+      setIsComplete(true);
       form.resetFields();
     });
   };
 
   return (
     <>
-      <Button className='da-btn add-account-button' type="primary" onClick={showModal}>Add Account</Button>
+      <Button className='da-btn add-account-button' type="primary" onClick={showModal}>{isFirst ? "Create Account" : "Add Account"}</Button>
       <Modal
         open={isModalOpen}
         title="Create New Account"
         okText="Create"
         onCancel={hideModal}
-        onOk={handleCreate}
+        // onOk={handleCreate}
+        footer={
+            <div style={{ display: "flex", justifyContent: "flex-end" }}>
+              <Button onClick={hideModal} style={{ marginRight: 8, display: isComplete ? "none": "flex" }}>Cancel</Button>
+              <Button onClick={handleCreate} style={{ marginRight: 8, display: isComplete ? "none": "flex" }}>Create</Button>
+              <Button type="primary" onClick={hideModal} style={{ display: isComplete ? "flex" : "none" }}>OK</Button>
+            </div>
+          }
       >
+        {isComplete ?
+        <Result
+            icon={<SmileOutlined />}
+            title="Congratulations, your account has been successfully created!"
+        />
+        :
         <Form form={form} layout="vertical">
           <Form.Item
             className='da-acc-create-form-input'
@@ -60,6 +76,7 @@ function NewAccountModal({ onCreate }) {
             <Input width='' maxLength={15} placeholder="Enter account name" />
           </Form.Item>
         </Form>
+        }
       </Modal>
     </>
 
@@ -68,6 +85,7 @@ function NewAccountModal({ onCreate }) {
 
 export default function DashboardAccounts() {
   const [isAccountAdd, setIsAccountAdd] = useState(true);
+  const [isNoAccounts, setIsNoAccounts] = useState(true);
   const { authUser: { uid } } = useAuth();
   const {
     accountsArr,
@@ -77,6 +95,14 @@ export default function DashboardAccounts() {
     updateCurrentAccountName,
     updateAccountsIds,
   } = useDash();
+
+
+  useEffect(() => {
+
+    if(!accountsArr?.length) {
+        setIsNoAccounts(false);
+    }
+  });
 
   const handleCreateAccount = async (values) => {
     const accountId = await accountManager.addAccount(values.accountName.trim(), uid);
@@ -100,23 +126,34 @@ export default function DashboardAccounts() {
 
   return (
     <>
-      <div className="dashboard-accounts-header">
-        <div className="dashboard-accounts-header-currentAccount">
-          <h1>{currentAccountName ? currentAccountName : "No Accounts"} </h1>
-        </div>
-        {accountsArr.length ?
-          <div className="dashboard-accounts-header-options">
-            <AccountsDrowpdown accountName={currentAccountName} onSelect={handleAccountSelect} accountAdded={{ isAccountAdd, setIsAccountAdd }} />
-            <NewAccountModal onCreate={handleCreateAccount} />
-          </div>
-          :
-          <div>
-            <NewAccountModal onCreate={handleCreateAccount} />
-          </div>
-            }
+        {
+            accountsArr?.length && isNoAccounts
+        ?
+        <>
+            <div className="dashboard-accounts-header">
+                <div className="dashboard-accounts-header-currentAccount">
+                <h1>{currentAccountName ? currentAccountName : "No Accounts"} </h1>
+                </div>
+                {accountsArr.length ?
+                <div className="dashboard-accounts-header-options">
+                    <AccountsDrowpdown accountName={currentAccountName} onSelect={handleAccountSelect} accountAdded={{ isAccountAdd, setIsAccountAdd }} />
+                    <NewAccountModal onCreate={handleCreateAccount}  setIsNoAccounts={setIsNoAccounts}  />
+                </div>
+                :
+                <div>
+                    <NewAccountModal onCreate={handleCreateAccount} setIsNoAccounts={setIsNoAccounts} />
+                </div>
+                    }
 
-      </div>
-      <DashboardAccountCard></DashboardAccountCard>
+            </div>
+            <DashboardAccountCard></DashboardAccountCard>
+        </>
+        :
+            <div className='dashboard-no-accounts'>
+                <h1 className='dashboard-no-accounts-heading'><span>Oops!</span> Looks like you don't have an account yet.</h1>
+                <NewAccountModal onCreate={handleCreateAccount} isFirst={true} setIsNoAccounts={setIsNoAccounts} />
+            </div>
+        }
     </>);
 }
 
